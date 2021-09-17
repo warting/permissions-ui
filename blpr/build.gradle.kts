e641bf8e7a5644b3b53d04dd1213053600233648
warting <stefan@warting.se>
2021-09-17 16:03:09 +0200

    don't minify

--- blpr/build.gradle.kts
+++ blpr/build.gradle.kts
@@ -41,11 +41,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            isMinifyEnabled = false
         }
     }
     compileOptions {
