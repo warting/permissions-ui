23eae3e366890c041dfb908e114d32557e5f888c
Stefan Wärting <stefan@warting.se>
2021-10-17 07:41:08 +0200

    deploy snapshots + remove copyright notice in headers (#37)

--- permissionsui/build.gradle.kts
+++ permissionsui/build.gradle.kts
@@ -1,26 +1,4 @@
-/*
- * MIT License
- *
- * Copyright (c) 2021 Stefan Wärting
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+
 
 plugins {
     id("com.android.library")
@@ -28,17 +6,11 @@ plugins {
     id("maven-publish")
     id("signing")
     id("org.jetbrains.dokka") version "1.5.31"
-    id("com.gladed.androidgitversion") version "0.4.14"
-}
-
-
-androidGitVersion {
-    tagPattern = "^v[0-9]+.*"
 }
 
 
 val PUBLISH_GROUP_ID: String by extra("se.warting.permissionsui")
-val PUBLISH_VERSION: String by extra(androidGitVersion.name().replace("v", ""))
+val PUBLISH_VERSION: String by extra(rootProject.version as String)
 val PUBLISH_ARTIFACT_ID by extra("permissionsui")
 
 apply(from = "${rootProject.projectDir}/gradle/publish-module.gradle")
