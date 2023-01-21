08b9e53f391071adf97d54b345463f8ada6e5e30
warting <stefan@warting.se>
2023-01-21 13:36:37 +0100

    fix detekt issues

--- permissionsui/src/main/java/se/warting/permissionsui/backgroundlocation/LocationInBackgroundTutorialView.kt
+++ permissionsui/src/main/java/se/warting/permissionsui/backgroundlocation/LocationInBackgroundTutorialView.kt
@@ -329,7 +329,11 @@ private fun openSettingsForApp(context: Context) {
 private fun getApplicationName(context: Context): String {
     val applicationInfo = context.applicationInfo
     val stringId = applicationInfo.labelRes
-    return if (stringId == 0) applicationInfo.nonLocalizedLabel.toString() else context.getString(
+    return if (stringId == 0) {
+        applicationInfo.nonLocalizedLabel.toString()
+    } else {
+        context.getString(
         stringId
     )
+    }
 }
